57b47808054b0a6e4cd1f27e33a2aedc8079cf82
boliso <102222437+boliso@users.noreply.github.com>
2022-06-29 10:14:13 +0100

    Create train.js

---
+++ train.js
@@ -0,0 +1,17 @@
+class Train {
+  constructor() {
+    this.r = 150;
+    this.x = width;
+    this.y = height - this.r;
+  }
+
+  move() {
+    this.x -= 16;
+  }
+
+  show() {
+    image(tImg, this.x, this.y, this.r, this.r);
+
+ 
+  }
+}
